524e9564900f7366a58c0bde58d0461be9ad7aae
Vivek Kondpalle <vukondpalle@gmail.com>
2024-05-01 19:33:51 +0530

    Restaurant Dashboard Repo initialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
@@ -37,6 +38,7 @@ export default function Home() {
           height={37}
           priority
         />
+        <Button>Click me</Button>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
